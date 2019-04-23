cde5a767e764e5cc25b8c7687b664c38140cc853
tenker <gxnulbj@126.com>
2019-04-23 21:07:21 +0800

    12

--- ksxt/HandlerPublicFun.ashx.cs
+++ ksxt/HandlerPublicFun.ashx.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Web;
 using System.Web.SessionState;
 using System.Text.RegularExpressions;
+using System.Data;
 
 namespace ksxt
 {
@@ -82,8 +83,7 @@ namespace ksxt
             if (context.Request.QueryString["logonUserType"] != null)
                 context.Session["logonUserType"] = context.Request.QueryString["logonUserType"].ToString();
             WriteResponse(context, 0, "操作成功", "");
-        }                    
-               
+        }               
 
         private void UpdateAnswerList(HttpContext context)
         {
@@ -93,37 +93,128 @@ namespace ksxt
             string type="";
             string  titleid="";
             string value ="";
+            string valuetype = "";
 
-            MatchCollection matchCollection = Regex.Matches(answerStr, "^(?<type>\\S+)_(?<titleid>\\d+)_answer_(?<value>\\d+)$");
+            string regStr = "^(?<type>\\S+)_(?<titleid>\\d+)_(?<valtype>answer|score)(?!_(?<value>\\d+))?$";
+            MatchCollection matchCollection = Regex.Matches(answerStr, regStr);
             if (matchCollection.Count > 0) {
                 type = matchCollection[0].Groups["type"].Value;
                 titleid = matchCollection[0].Groups["titleid"].Value;
                 value = matchCollection[0].Groups["value"].Value;
-            }                      
-           
-            string sqlFormat,sql;
-            int code;
-
+                valuetype= matchCollection[0].Groups["valtype"].Value;
+            }                                
             if (user_id != "") {
-
-                sqlFormat = "delete from tb_answer_list where user_id={0} and title_list_id={1} ";
-                sql = string.Format(sqlFormat, user_id, titleid);
-
-                code = ExecuteNoQuery(sql);
-                if (code < 0) {
-                    WriteResponse(context, -1, dbError);
+                if (valuetype == "score") {
+                    UpdateScore(context, titleid, user_id, answer);
                     return;
                 }
+                else {
+                    //先删除原来项
+                    string sqlFormat = "delete from tb_answer_list where user_id={0} and title_list_id={1} ";
+                    string sql = string.Format(sqlFormat, user_id, titleid);
+
+                    int code = ExecuteNoQuery(sql);
+                    if (code < 0) {
+                        WriteResponse(context, -1, dbError);
+                        return;
+                    }
+                    if (type == "choice") {
+                        InsertAnswerChoice(context, titleid, user_id, value);
+                    }
+                    else if (type == "judge") {
+                        InsertAnswerJudge(context, titleid, user_id, value);
+                    }
+                    else if (type == "filling") {
+                        InsertAnswerFilling(context, titleid, user_id, answer);
+                    }
+                    else if (type == "qa") {
+                        InsertAnswerQa(context, titleid, user_id, answer);
+                    }
+                }
             }
-            // 插入答案
-            sqlFormat = @"insert into tb_answer_list(title_list_id,user_id,value)values({0},{1},{2})";
-            sql = string.Format(sqlFormat, titleid, user_id, value);
-            code = ExecuteNoQuery(sql);
+            else {
+                WriteResponse(context, -1, "登录超时,请重新登录进行操作!");
+            }
+           
+        } 
+        
+        private void InsertAnswerChoice(HttpContext context,string title_list_id,string user_id,string value)
+        {
+            DataTable dt =  ExecuteQueryData(string.Format(@"SELECT p.choice_score,c.answer_arry
+                                                FROM tb_title_list as t
+                                                INNER JOIN tb_choice as c on t.title_id = c.id
+                                                INNER JOIN tb_paper as p on p.id = t.paper_id
+                                                WHERE t.type = 'choice' and t.title_list_id ={0}", title_list_id));
+            int score = 0;
+            if (dt.Rows.Count > 0) {
+                if(dt.Rows[0]["answer_arry"].ToString()==value) {
+                    int.TryParse(dt.Rows[0]["choice_score"].ToString(), out score);
+                }
+            }
+            string sqlFormat = @"insert into tb_answer_list(title_list_id,user_id,value,score)values({0},{1},{2},{3})";
+            string sql = string.Format(sqlFormat, title_list_id, user_id, value,score);
+            int code = ExecuteNoQuery(sql);
             if (code < 0)
                 WriteResponse(context, -1, dbError);
             else
                 WriteResponse(context, 0);
-        }                
+        }
+
+        private void InsertAnswerJudge(HttpContext context, string title_list_id, string user_id, string value)
+        {
+            DataTable dt = ExecuteQueryData(string.Format(@"SELECT p.judge_score,c.answer_arry
+                                                FROM tb_title_list as t
+                                                INNER JOIN tb_choice as c on t.title_id = c.id
+                                                INNER JOIN tb_paper as p on p.id = t.paper_id
+                                                WHERE t.type = 'judge' and t.title_list_id ={0}", title_list_id));
+            int score = 0;
+            if (dt.Rows.Count > 0) {
+                if (dt.Rows[0]["answer_arry"].ToString() == value) {
+                    int.TryParse(dt.Rows[0]["judge_score"].ToString(), out score);
+                }
+            }
+            string sqlFormat = @"insert into tb_answer_list(title_list_id,user_id,value,score)values({0},{1},{2},{3})";
+            string sql = string.Format(sqlFormat, title_list_id, user_id, value,score);
+            int code = ExecuteNoQuery(sql);
+            if (code < 0)
+                WriteResponse(context, -1, dbError);
+            else
+                WriteResponse(context, 0);
+        }
+        
+        private void InsertAnswerFilling(HttpContext context, string title_list_id, string user_id, string value)
+        {
+            string sqlFormat = @"insert into tb_answer_list(title_list_id,user_id,value)values({0},{1},{2})";
+            string sql = string.Format(sqlFormat, title_list_id, user_id, value);
+            int code = ExecuteNoQuery(sql);
+            if (code < 0)
+                WriteResponse(context, -1, dbError);
+            else
+                WriteResponse(context, 0);
+        }
+
+        private void InsertAnswerQa(HttpContext context, string title_list_id, string user_id, string value)
+        {
+            string sqlFormat = @"insert into tb_answer_list(title_list_id,user_id,value)values({0},{1},{2})";
+            string sql = string.Format(sqlFormat, title_list_id, user_id, value);
+            int code = ExecuteNoQuery(sql);
+            if (code < 0)
+                WriteResponse(context, -1, dbError);
+            else
+                WriteResponse(context, 0);
+        }
+
+        private void UpdateScore(HttpContext context, string title_list_id, string user_id,string score)
+        {
+            string sqlFormat = @"update tb_answer_list set score={0} where title_list_id={1} and user_id={2}";
+            string sql = string.Format(sqlFormat, score,title_list_id, user_id);
+            int code = ExecuteNoQuery(sql);
+
+            if (code < 0)
+                WriteResponse(context, -1, dbError);
+            else
+                WriteResponse(context, 0);
+        }
 
         private void ChangePassword(HttpContext context)
         {
